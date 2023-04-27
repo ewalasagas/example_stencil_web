927e5d49a50cadca57693d6f5e9340648e9b419f
Elaine Alasagas <ealasagas@gmail.com>
2023-04-26 23:16:44 -0700

    adding menu links

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -8,7 +8,11 @@ import { Component, Prop, h } from '@stencil/core';
 
 export class SideDrawer {
     @Prop({reflect: true}) title: string;
-    @Prop({reflect: true}) open: boolean;
+    @Prop({reflect: true, mutable: true}) open: boolean;
+
+    onCloseDrawer() {
+        this.open = false;
+    }
 
     render() {
         // let content = null;
@@ -22,13 +26,36 @@ export class SideDrawer {
         //         </aside>
         //     )
         // }
+
+        let mainContent = <slot />
+        mainContent = (
+            <div>
+            <h2>Contact Information</h2>
+            <p>You can reach us via phone or email.</p>
+            <ul>
+                <li>Phone: 555-555-5555</li>
+                <li>
+                    Email: {' '}
+                    <a href="mailto:something@something.com">testemail@testemail.com</a>
+                </li>
+            </ul>
+            </div>
+        );
+        
         
         return (
             <aside>   
-            <header><h1>{this.title}</h1></header>
-            <main>
-                <slot />
-            </main>
+                <header>
+                    <h1>{this.title}</h1>
+                    <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+                </header>
+                <section id="tabs">
+                    <button class="active">Navigation</button>
+                    <button>Contact</button>
+                </section>
+                <main>
+                    <slot />
+                </main>
             </aside>
         );
     }
